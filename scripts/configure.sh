41c6281837ad55029aec0cd72bf48a4cb578483c
rayanfer32 <rayanfernandes9@gmail.com>
2026-02-07 15:49:15 +0530

    feat: add arm support

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -10,13 +10,13 @@ sudo apt install -y xfce4 xfce4-goodies xfonts-base xubuntu-icon-theme xubuntu-w
 # * 2, install TurboVNC
 # Fun Fact: TurboVNC is the only VNC implementations that supports OpenGL acceleration without an graphics device by default
 # By the way, you can still use the legacy version of this script where instead of installing TurboVNC, tightvncserver is installed.
-# wget https://phoenixnap.dl.sourceforge.net/project/turbovnc/2.2.5/turbovnc_2.2.5_amd64.deb
-sudo dpkg -i turbovnc_2.2.5_amd64.deb
+wget https://phoenixnap.dl.sourceforge.net/project/turbovnc/3.1/turbovnc_3.1_arm64.deb
+sudo dpkg -i turbovnc_3.1_arm64.deb
 
 # * 3, download ngrok
 # wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
-tar xzf ngrok-v3-stable-linux-amd64.tgz
-chmod +x ngrok
+# tar xzf ngrok-v3-stable-linux-amd64.tgz
+# chmod +x ngrok
 
 # * 4, generate and copy passwd file and xstartup script
 export PATH=$PATH:/opt/TurboVNC/bin
@@ -26,7 +26,7 @@ echo $VNC_PASSWORD | vncpasswd -f > $HOME/.vnc/passwd
 chmod 0600 $HOME/.vnc/passwd
 
 # * 5, set up auth token from argument
-./ngrok config add-authtoken $NGROK_AUTH_TOKEN
+# ./ngrok config add-authtoken $NGROK_AUTH_TOKEN
 
 # * 6, restore firefox profile
 rm -rf ~/.mozilla
--- scripts/setup-rustdesk.sh
+++ scripts/setup-rustdesk.sh
@@ -2,8 +2,8 @@
 
 # * install rustdesk
 echo "Installing RustDesk..."
-# wget https://github.com/rustdesk/rustdesk/releases/download/1.4.4/rustdesk-1.4.4-x86_64.deb > /dev/null 2>&1
-sudo apt install -fy ./rustdesk*.deb
+wget https://github.com/rustdesk/rustdesk/releases/download/1.4.5/rustdesk-1.4.5-aarch64.deb > /dev/null 2>&1
+sudo apt install -fy ./rustdesk-1.4.5-aarch64.deb
 
 # * Ensure we run RustDesk in the user account (runner) on the VNC display
 export DISPLAY=${DISPLAY:-:1}
