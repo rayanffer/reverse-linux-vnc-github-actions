b701212e1f55245df4859514ae9dbe57a4cfd39e
rayanffer <84654621+rayanffer@users.noreply.github.com>
2026-02-09 12:16:22 +0530

    Set DISPLAY environment variable for Firefox

--- scripts/setup-user-services.sh
+++ scripts/setup-user-services.sh
@@ -4,6 +4,6 @@
 # wget https://gist.github.com/rayanfer32/8b16357e97cf1504b7ddee8df0b3c00c/raw/tap.userscript.user.js
 
 # start the browser for ai providers script
-firefox https://example.com &
+export DISPLAY=:1; firefox https://example.com &
 
 exit 0
